11d965e971babb32ca124d57403d82ac72d8addb
abutan <abutan@mail.ru>
2020-03-07 15:16:47 +0300

    Reviews

--- js/main.js
+++ js/main.js
@@ -13,3 +13,49 @@
         $('#sert-popup').fancybox();
     });
 })(jQuery);
+
+(function ($) {
+    $('.reviews-slider').slick({
+        accessibility: true,
+        lazyLoad: 'ondemand',
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+})(jQuery);
