0509566310608b5a51cdb059f3ea7d048907eaba
abutan <abutan@mail.ru>
2020-03-09 20:42:17 +0300

    Schedule

--- js/main.js
+++ js/main.js
@@ -94,3 +94,72 @@
         $(this).siblings().find('.pre-img').css('background', 'url("./images/default/arrow_down.png") no-repeat center 3px');
     })
 })(jQuery);
+
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        let calendarEl = document.getElementById('calendar');
+
+        let calendar = new FullCalendar.Calendar(calendarEl, {
+            locale: 'ru',
+            plugins: ['interaction', 'dayGrid', 'timeGrid'],
+            defaultView: 'dayGridMonth',
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+            events: [
+                {
+                    title: 'Занятия йогой',
+                    start: '2020-02-01'
+                },
+                {
+                    title: 'Заниятия фитнесом',
+                    start: '2020-02-07',
+                    end: '2020-02-10'
+                },
+                {
+                    groupId: '999',
+                    title: 'Занятия бизнес фитнесом',
+                    start: '2020-02-09T16:00:00'
+                },
+                {
+                    groupId: '999',
+                    title: 'Занятия бизнес фитнесом',
+                    start: '2020-02-16T16:00:00'
+                },
+                {
+                    title: 'Консалтинг по консалтингу',
+                    start: '2020-02-11',
+                    end: '2020-02-13'
+                },
+                {
+                    title: 'Встреча с преподователями',
+                    start: '2020-02-12T10:30:00',
+                    end: '2020-02-12T12:30:00'
+                },
+                {
+                    title: 'Бассейн',
+                    start: '2020-02-12T12:00:00'
+                },
+                {
+                    title: 'Митинг благодарных пользователеей',
+                    start: '2020-02-12T14:30:00'
+                },
+                {
+                    title: 'Праздник годовщины',
+                    start: '2020-02-13T07:00:00'
+                },
+                {
+                    title: 'Смотрим что то',
+                    url: 'http://google.com/',
+                    start: '2020-02-28'
+                }
+            ]
+        });
+
+        calendar.setOption('locale', 'ru');
+
+        calendar.render();
+    });
+})();
